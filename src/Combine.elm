4c8a31617f1156859e413187a0f68ce0dfdb5172
Bogdan Popa <popa.bogdanp@gmail.com>
2015-10-21 12:03:00 +0300

    keep track of position in context

--- src/Combine.elm
+++ src/Combine.elm
@@ -7,6 +7,7 @@ import Regex exposing (Regex(..))
 
 type alias Context =
   { input : String
+  , position : Int
   }
 
 
@@ -24,6 +25,7 @@ type Parser res
   | RecursiveParser (L.Lazy (ParseFn res))
 
 
+{-| Unwrap a parser so it can be applied to a context. -}
 app : Parser res -> (Context -> (Result res, Context))
 app p =
   case p of
@@ -36,33 +38,38 @@ app p =
 
 {-| Parse a string. -}
 parse : Parser res -> String -> (Result res, Context)
-parse p input = app p { input = input }
+parse p input = app p { input = input, position = 0 }
 
 
 {-| Defer a parser's evaluation.
 
     type E = ETerm String | EList (List E)
 
+    whitespace : Parser String
     whitespace = regex "[ \t\r\n]*"
+
+    term : Parser E
     term = ETerm `map` (whitespace *> regex "[a-zA-Z]+" <* whitespace)
+
+    list : Parser E
     list = rec (\() -> EList `map` (string "(" *> many (term `or` list) <* string ")"))
 
     parse list "" == \
-      (Fail "expected '('", { input = "" })
+      (Fail "expected '('", { input = "", position = 0 })
 
     parse list "()" == \
-      (Done (EList []), { input = "" })
+      (Done (EList []), { input = "", position = 2 })
 
     parse list "(a (b c))" == \
-      (Done (EList [ETerm "a", EList [ETerm "b", ETerm "c"]]), { input = "" })
+      (Done (EList [ETerm "a", EList [ETerm "b", ETerm "c"]]) \
+      , { input = "", position = 9 })
  -}
 rec : (() -> Parser res) -> Parser res
 rec t =
   RecursiveParser << L.lazy <| \() -> app (t ())
 
 
-{-| Transform both the result and error message of a parser.
- -}
+{-| Transform both the result and error message of a parser. -}
 bimap : (res -> res')
       -> (String -> String)
       -> Parser res
@@ -80,7 +87,7 @@ bimap fok ferr p =
 {-| Transform the result of a parser.
 
     parse (map String.toUpper (string "a")) "a" == \
-      (Done "A", { input = "" })
+      (Done "A", { input = "", position = 1 })
  -}
 map : (res -> res') -> Parser res -> Parser res'
 map f p = bimap f identity p
@@ -101,6 +108,19 @@ andThen p f =
 
 
 {-| Sequence two parsers.
+
+    import Maybe
+    import Result
+    import String
+
+    num : Parser Int
+    num = (Maybe.withDefault 0 << Result.toMaybe << String.toInt) `map` regex "[0-9]+"
+
+    sum : Parser Int
+    sum = (+) `map` (num <* string "+") `andMap` num
+
+    parse sum "1+2" == \
+      (Done 3, { input = "", position = 3 })
  -}
 andMap : Parser (res -> res') -> Parser res -> Parser res'
 andMap lp rp =
@@ -112,13 +132,13 @@ andMap lp rp =
 {-| Choose between two parsers.
 
     parse (string "a" `or` string "b") "a" == \
-      (Done "a", { input = "" })
+      (Done "a", { input = "", position = 1 })
 
     parse (string "a" `or` string "b") "b" == \
-      (Done "b", { input = "" })
+      (Done "b", { input = "", position = 1 })
 
     parse (string "a" `or` string "b") "c" == \
-      (Fail "expected 'a' or expected 'b'", { input = "c" })
+      (Fail "expected 'a' or expected 'b'", { input = "c", position = 0 })
  -}
 or : Parser res -> Parser res -> Parser res
 or lp rp =
@@ -142,10 +162,10 @@ or lp rp =
 {-| Choose between a list of parsers.
 
     parse (choice [string "a", string "b"]) "a" == \
-      (Done "a", { input = "" })
+      (Done "a", { input = "", position = 1 })
 
     parse (choice [string "a", string "b"]) "b" == \
-      (Done "b", { input = "" })
+      (Done "b", { input = "", position = 1 })
  -}
 choice : List (Parser res) -> Parser res
 choice xs =
@@ -155,10 +175,10 @@ choice xs =
 {-| Apply a parser until it fails and return a list of the results.
 
     parse (many (string "a")) "aaab" == \
-      (Done ["a", "a", "a"], { input = "b" })
+      (Done ["a", "a", "a"], { input = "b", position = 3 })
 
     parse (many (string "a")) "" == \
-      (Done [], { input = "" })
+      (Done [], { input = "", position = 0 })
  -}
 many : Parser res -> Parser (List res)
 many p =
@@ -179,10 +199,10 @@ many p =
 {-| Parse at least one result.
 
     parse (many1 (string "a")) "a" == \
-      (Done ["a"], { input = "" })
+      (Done ["a"], { input = "", position = 1 })
 
     parse (many1 (string "a")) "" == \
-      (Fail "expected 'a'", { input = "" })
+      (Fail "expected 'a'", { input = "", position = 0 })
  -}
 many1 : Parser res -> Parser (List res)
 many1 p =
@@ -199,7 +219,7 @@ fail m =
 {-| Return a value without consuming any input.
 
     parse (succeed 1) "a" == \
-      (Done 1, { input = "a" })
+      (Done 1, { input = "a", position = 0 })
  -}
 succeed : res -> Parser res
 succeed r =
@@ -210,16 +230,21 @@ succeed r =
 {-| Parse an exact string match.
 
     parse (string "hello") "hello world" == \
-      (Done "hello", { input = " world" })
+      (Done "hello", { input = " world", position = 5 })
 
     parse (string "hello") "goodbye" == \
-      (Fail "expected 'hello'", { input = "goodbye" })
+      (Fail "expected 'hello'", { input = "goodbye", position = 0 })
  -}
 string : String -> Parser String
 string s =
   Parser <| \c ->
     if String.startsWith s c.input
-    then (Done s, {c | input <- String.dropLeft (String.length s) c.input})
+    then
+      let
+        len = String.length s
+        rem = String.dropLeft len c.input
+        pos = c.position + len
+      in (Done s, {c | input <- rem, position <- pos})
     else (Fail ("expected '" ++ s ++ "'"), c)
 
 
@@ -230,7 +255,7 @@ subgroups are ignored. A `^` is added implicitly to the beginning of
 every pattern unless one already exists.
 
     parse (regex "a+") "aaaaab" == \
-      (Done "aaaaa", { input = "b" })
+      (Done "aaaaa", { input = "b", position = 5 })
  -}
 regex : String -> Parser String
 regex pattern =
@@ -243,8 +268,11 @@ regex pattern =
   Parser <| \c ->
     case Regex.find (Regex.AtMost 1) (Regex.regex pattern') c.input of
       [match] ->
-        let rem = String.dropLeft (String.length match.match) c.input
-        in (Done match.match, {c | input <- rem })
+        let
+          len = String.length match.match
+          rem = String.dropLeft len c.input
+          pos = c.position + len
+        in (Done match.match, {c | input <- rem, position <- pos })
 
       _ ->
         (Fail ("expected input matching Regexp /" ++ pattern' ++ "/"), c)
@@ -253,16 +281,20 @@ regex pattern =
 {-| Consume input while the predicate matches.
 
     parse (while ((/=) ' ')) "test 123" == \
-      (Done "test", { input = " 123" })
+      (Done "test", { input = " 123", position = 4 })
  -}
 while : (Char -> Bool) -> Parser String
-while p =
+while pred =
   let
     accumulate acc c =
       case String.uncons c.input of
         Just (h, rest) ->
-          if p h
-          then accumulate (acc ++ (String.left 1 c.input)) { c | input <- rest }
+          if pred h
+          then
+            let
+              char = String.cons h ""
+              pos = c.position + 1
+            in accumulate (acc ++ char) { c | input <- rest, position <- pos }
           else (acc, c)
 
         Nothing ->
@@ -275,8 +307,8 @@ while p =
 
 {-| Fail when the input is not empty.
 
-    parse end "" == (Done (), { input = "" })
-    parse end "a" == (Fail "expected end of input", { input = "a" })
+    parse end "" == (Done (), { input = "", position = 0 })
+    parse end "a" == (Fail "expected end of input", { input = "a", position = 0 })
  -}
 end : Parser ()
 end =
@@ -291,8 +323,8 @@ end =
     letterA : Parser String
     letterA = optional (string "a") "a"
 
-    parse letterA "a" == (Done "a", { input = "" })
-    parse letterA "b" == (Done "a", { input = "b" })
+    parse letterA "a" == (Done "a", { input = "", position = 1 })
+    parse letterA "b" == (Done "a", { input = "b", position = 0 })
  -}
 optional : Parser res -> res -> Parser res
 optional p res =
@@ -304,7 +336,7 @@ optional p res =
     unsuffix : Parser String
     unsuffix = regex "[a-z]" <* regex "[!?]"
 
-    parse unsuffix "a!" == (Done "a", { input = "" })
+    parse unsuffix "a!" == (Done "a", { input = "", position = 2 })
  -}
 (<*) : Parser res -> Parser x -> Parser res
 (<*) lp rp =
@@ -316,7 +348,7 @@ optional p res =
     unprefix : Parser String
     unprefix = string ">" *> while ((==) ' ') *> while ((/=) ' ')
 
-    parse unprefix "> a" == (Done "a", { input = "" })
+    parse unprefix "> a" == (Done "a", { input = "", position = 3 })
  -}
 (*>) : Parser x -> Parser res -> Parser res
 (*>) lp rp =
